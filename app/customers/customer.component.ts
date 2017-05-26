8397bc200fffbea1ece24952a07f8509e7ef7ed8
h1416 <hieuvo1416@gmail.com>
2017-05-26 06:59:59 -0500

    Import validators for form validation

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
