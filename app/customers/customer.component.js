b51f137085acc4c0f07d4f2081590befdcaad074
h1416 <hieuvo1416@gmail.com>
2017-05-30 07:26:47 -0500

    use validatorFn to build a custom range validator

--- app/customers/customer.component.js
+++ app/customers/customer.component.js
@@ -11,14 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var forms_1 = require('@angular/forms');
 var customer_1 = require('./customer');
-function ratingRange(c) {
-    if (c.value != undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
-        return { 'range': true };
-    }
-    ;
-    return null; // if the validation passes
+function ratingRange(min, max) {
+    return function (c) {
+        if (c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return { 'range': true }; // something is wrong
+        }
+        ;
+        return null; // if the validation passes
+    };
 }
-;
 var CustomerComponent = (function () {
     function CustomerComponent(formBuilder) {
         this.formBuilder = formBuilder;
@@ -31,7 +32,7 @@ var CustomerComponent = (function () {
             email: ['', [forms_1.Validators.required, forms_1.Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
             phone: '',
             notification: 'email',
-            rating: ['', ratingRange],
+            rating: ['', ratingRange(1, 5)],
             sendCatalog: true
         });
     };
